9bc02cf0f84b389947c7786559aa3f31daa832d3
Irvin Etori <irvin.etori@gmail.com>
2023-03-09 11:41:06 +0100

    chart 85%

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+  get 'chart', to: 'pages#chart'
   resources :portfolios, only: %i[index show create update] do
     resources :transactions, only: :index
   end
